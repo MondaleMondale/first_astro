3616c849eb212ead5fa9a4d739a84cd9ed327aa9
MondaleMondale <klmh@kea.dk>
2023-01-22 23:04:34 +0100

    Fixed rss reader

--- src/pages/rss.xml.js
+++ src/pages/rss.xml.js
@@ -1,11 +1,12 @@
-// import rss, { pagesGlobToRssItems } from "@astrojs/rss";
+import rss, { pagesGlobToRssItems } from "@astrojs/rss";
 
-// export async function get() {
-//   return rss({
-//     title: "Astro Learner | Blog",
-//     description: "My journey learning Astro",
-//     site: "https://master--my-first-astroo.netlify.app",
-//     items: await pagesGlobToRssItems(import.meta.glob("./**/*.md")),
-//     customData: `<language>da-dk</language>`,
-//   });
-// }
+export async function get() {
+  return rss({
+    title: "Astro Learner | Blog",
+    description: "My journey learning Astro",
+    site: "https://master--my-first-astroo.netlify.app",
+    //  items: await pagesGlobToRssItems(import.meta.glob("./**/*.md")),
+    items: import.meta.glob("./**/*.md"),
+    customData: `<language>da-dk</language>`,
+  });
+}
